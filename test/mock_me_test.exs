61a24b4932415244670279cf0af2a943373ecf38
Nathan Davis <nathan@nathan.salesrabbit>
2020-12-15 09:20:54 -0700

    adds more complete tests

--- test/mock_me_test.exs
+++ test/mock_me_test.exs
@@ -3,24 +3,71 @@ defmodule MockMeTest do
   use ExUnit.Case
   doctest MockMe
 
-  test "has a state agent started" do
-    assert MockMe.set_response(:jwt, :failure)
-    assert MockMe.flag_value(:jwt) == :failure
-  end
+  describe "state agent" do
+    test "has started" do
+      assert MockMe.set_response(:jwt, :failure)
+      assert MockMe.flag_value(:jwt) == :failure
+    end
 
-  test "reset_flags/0 resets state" do
-    assert MockMe.set_response(:test_me, :wipe_me)
+    test "set_response/2" do
+      assert MockMe.set_response(:test_me, :failure)
 
-    assert MockMe.flag_value(:test_me) == :wipe_me
+      assert MockMe.flag_value(:test_me) == :failure
+    end
 
-    assert MockMe.reset_flags()
-    assert MockMe.flag_value(:test_me) == :success
+    test "set_response/2 ignores unset flags" do
+      assert MockMe.set_response(:no_flag, :bogus)
+
+      assert MockMe.flag_value(:no_flag) == :bogus
+    end
+
+    test "reset_flags/0" do
+      assert MockMe.set_response(:test_me, :wipe_me)
+
+      assert MockMe.flag_value(:test_me) == :wipe_me
+
+      assert MockMe.reset_flags()
+      assert MockMe.flag_value(:test_me) == :success
+    end
   end
 
   # Need to write the actual integration tests
+  describe "integrations with test endpoints" do
+    test "getting a success repsonse" do
+      assert MockMe.set_response(:test_me, :success)
 
-  test "some stuff" do
-    assert {:ok, %HTTPoison.Response{status_code: 200}} =
-             HTTPoison.get("http://localhost:9081/test-path")
+      assert {:ok, %HTTPoison.Response{status_code: 200, body: resp_body}} =
+               HTTPoison.get("http://localhost:9081/test-path")
+
+      assert "test-body" == resp_body
+    end
+
+    test "toggling the response" do
+      assert MockMe.set_response(:test_me, :failure)
+
+      assert {:ok, %HTTPoison.Response{status_code: 422, body: resp_body}} =
+               HTTPoison.get("http://localhost:9081/test-path")
+
+      assert "test-failure-body" == resp_body
+    end
+
+    test "invalid flag" do
+      assert MockMe.set_response(:test_me, :invalid_flag)
+
+      assert {:ok, %HTTPoison.Response{status_code: 500, body: resp_body}} =
+               HTTPoison.get("http://localhost:9081/test-path")
+
+      body = Jason.decode!(resp_body)
+      assert body["data"] != nil
+    end
+
+    test "not defined route" do
+      assert MockMe.set_response(:test_me, :invalid_flag)
+
+      assert {:ok, %HTTPoison.Response{status_code: 404, body: resp_body}} =
+               HTTPoison.get("http://localhost:9081/not-defined")
+
+      assert resp_body =~ "has not been defined"
+    end
   end
 end
